2e77ad420bc0339bcc0b83acba9dce0ffd4536ac
Paul Stanley <paulnstanley@github.com>
2019-10-11 13:46:48 -0400

    Updates

---
+++ precache-manifest.79c55849f74e5c2f3542bd0f195c3845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65789f81281dfd41506d0dfb518b8529",
+    "url": "/paul-test-app/index.html"
+  },
+  {
+    "revision": "4d45b154527adacd034f",
+    "url": "/paul-test-app/static/css/main.5029b547.chunk.css"
+  },
+  {
+    "revision": "528a976814d923971dc8",
+    "url": "/paul-test-app/static/js/2.a65331ff.chunk.js"
+  },
+  {
+    "revision": "4d45b154527adacd034f",
+    "url": "/paul-test-app/static/js/main.913aa776.chunk.js"
+  },
+  {
+    "revision": "875b75d668de2b927ae9",
+    "url": "/paul-test-app/static/js/runtime-main.f2d58b90.js"
+  },
+  {
+    "revision": "77334999242169bdad2985bee2a81254",
+    "url": "/paul-test-app/static/media/logo.77334999.png"
+  }
+]);
