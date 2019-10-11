296808efdf94c868619dcf6efa26ef572502c6b9
Paul Stanley <paulnstanley@github.com>
2019-10-11 13:42:40 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Paul's</h2>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Test Site</h2>
+        <h2>¯\_(ツ)_/¯ ¯\_(ツ)_/¯ ¯\_(ツ)_/¯</h2>
       </header>
+      <body>
+        <button id="testbutton1">¯\_(ツ)_/¯ Whoa</button>
+        <p></p>
+        <button id="testbutton2">¯\_(ツ)_/¯ No way</button>
+        <p></p>
+        <button id="testbutton3">¯\_(ツ)_/¯ So bad</button>
+      </body>
     </div>
   );
 }
